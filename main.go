c327a494ecae56faf8d906c141d33fef95e5b1f9
OmarEP <omrspz@gmail.com>
2024-12-28 16:02:26 -0500

    change reset and metrics endpoints to addmin

--- main.go
+++ main.go
@@ -3,20 +3,31 @@ package main
 import (
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
 	const filePathRoot = "."
 	const port = "8080"
 
+	apiCfg := &apiConfig{}
+
 	mux := http.NewServeMux()
+	fsHandler := apiCfg.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(filePathRoot))))
+	mux.Handle("/app/", fsHandler)
+	mux.HandleFunc("GET /api/healthz", handlerReadiness)
+
+	mux.HandleFunc("GET /admin/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("POST /admin/reset", apiCfg.handlerReset)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
-
-	mux.Handle("/", http.FileServer(http.Dir(filePathRoot)))
 
 	log.Printf("Serving files from %s on port: %s\n", filePathRoot, port)
 	log.Fatal(srv.ListenAndServe())
